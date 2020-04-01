3caf0e539db862b5ae5288e543c2095896655384
akulbaeva <aiymbubu.kulbaeva@iaau.edu.kg>
2020-04-01 19:08:02 +0600

    Security added

---
+++ src/main/java/readinglist/ReadingListController.java
@@ -0,0 +1,42 @@
+package readinglist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+    private ReadingListRepository readingListRepository;
+
+    @Autowired
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
+    @GetMapping(value = "/readingList")
+    public String readersBooks(String reader, Model model) {
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+
+    @PostMapping(value = "/readingList")
+    public String addToReadingList(String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/readingList";
+    }
+
+    @GetMapping(value = "/")
+    public String ind() {
+        return "redirect:/readingList";
+    }
+}
---
+++ src/test/java/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,22 @@
+package readinglist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+//import org.springframework.boot.test.SpringApplicationConfigure;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import readinglist.ReadingListApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ReadingListApplication.class)
+@WebAppConfiguration
+
+//@SpringBootTest
+class ReadingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
